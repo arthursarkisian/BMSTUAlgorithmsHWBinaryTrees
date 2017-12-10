aee3a700b4f793083b76f3febb26288a16f19f0c
Arthur Sarkisyan <arthurunique24@gmail.com>
2017-12-10 13:18:11 +0300

    All files

---
+++ cmake-build-debug/CMakeFiles/BinarTrees.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/Arthur/ClionProjects/BinarTrees/AA/AATree.cpp" "/Users/Arthur/ClionProjects/BinarTrees/cmake-build-debug/CMakeFiles/BinarTrees.dir/AA/AATree.cpp.o"
+  "/Users/Arthur/ClionProjects/BinarTrees/AVL/AVLTree.cpp" "/Users/Arthur/ClionProjects/BinarTrees/cmake-build-debug/CMakeFiles/BinarTrees.dir/AVL/AVLTree.cpp.o"
+  "/Users/Arthur/ClionProjects/BinarTrees/main.cpp" "/Users/Arthur/ClionProjects/BinarTrees/cmake-build-debug/CMakeFiles/BinarTrees.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/BinarTrees.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/BinarTrees.dir/main.cpp.o"
+  "CMakeFiles/BinarTrees.dir/AVL/AVLTree.cpp.o"
+  "CMakeFiles/BinarTrees.dir/AA/AATree.cpp.o"
+  "BinarTrees.pdb"
+  "BinarTrees"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BinarTrees.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
